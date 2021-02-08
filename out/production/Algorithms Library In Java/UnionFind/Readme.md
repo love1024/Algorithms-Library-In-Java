3f8d7f592f197c7542c9fd4a315048e726768cdb
lovepreetsingh <kaler1024@gmail.com>
2021-02-08 16:56:27 +0530

    added Union find with path compression

--- out/production/Algorithms Library In Java/UnionFind/Readme.md
+++ out/production/Algorithms Library In Java/UnionFind/Readme.md
@@ -1,4 +1,4 @@
-## Time Complexity Of Union Find
+## Time Complexity Of Worst Case Union Find
 __Initialize__: N  
-__Union__     : N  
-__Find__      : 1
+__Union__     : logN  
+__Find__      : logN
--- src/UnionFind/UnionFind.java
+++ src/UnionFind/UnionFind.java
@@ -1,18 +1,21 @@
 package UnionFind;
 
 /**
- * Union Find Data Structure For Dynamic Component
- * Connections
+ * Algorithm: Weighted Union Find + Path Compression
+ * Author   : Lovepreet Singh
  */
 public class UnionFind {
     private int[] parent;
+    private int[] sizes;
 
     public UnionFind(int N) {
         // Set parent of each element to itself
         // (N array accesses)
         parent = new int[N];
+        sizes = new int[N];
         for (int i=0; i<N; i++) {
             parent[i] = i;
+            sizes[i] = 1;
         }
     }
 
@@ -20,6 +23,13 @@ public class UnionFind {
         // Check parent pointers until reach root
         // (Depth of i array accesses)
         while (parent[p] != p) {
+
+            //PATH COMPRESSION
+            //No need to traverse path all time
+            //Just make it point to grandparent instead of parent
+            //Halving the length to traverse, as we only care about root
+            parent[p] = parent[parent[p]];
+
             p = parent[p];
         }
         return p;
@@ -37,6 +47,16 @@ public class UnionFind {
         int p_root = this.root(p);
         int q_root = this.root(q);
 
-        this.parent[p_root] = q_root;
+        // If they already under same parent, no need to do anything
+        if(p_root == q_root) return;
+
+        // If
+        if(sizes[p_root] < sizes[q_root]) {
+            this.parent[p_root] = q_root;
+            sizes[q_root] += sizes[p_root];
+        } else {
+            this.parent[q_root] = p_root;
+            sizes[p_root] += sizes[q_root];
+        }
     }
 }
