28477d7e4bc9b7c7323a0222fbbd765eadbf8c18
lovepreetsingh <kaler1024@gmail.com>
2021-02-15 15:54:15 +0530

    Added binary search

---
+++ src/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package search;
+
+public class BinarySearch {
+    public static int binarySearch(int[] a, int key) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (key < a[mid]) {
+                high = mid - 1;
+            } else if (key > a[mid]) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 3, 5, 7, 9};
+
+        int idx = binarySearch(a, 5);
+        System.out.println(idx);
+    }
+}
